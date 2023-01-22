d201a18a0a0ee7fcaa147af804361760d6231747
amjed-ali-k <amjedmgm@gmail.com>
2023-01-22 12:43:46 +0530

    fix: updae

--- app/database/recipies.py
+++ app/database/recipies.py
@@ -1,32 +1,29 @@
-from pymongo import MongoClient
 from bson import ObjectId
-from decouple import config
-
-connection_details = config("DB_HOST")
-
-client = MongoClient(connection_details)
+from . import client
 
 database = client.recipes
 
-recipe_collection = database.get_collection('recipes_collection')
+recipe_collection = database.get_collection("recipes_collection")
+
 
 def parse_recipe_data(recipe) -> dict:
     return {
         "id": str(recipe["_id"]),
         "name": recipe["name"],
-        "ingredients": recipe["ingredients"]
+        "ingredients": recipe["ingredients"],
     }
+
 
 def save_recipe(recipe_data: dict) -> dict:
     recipe = recipe_collection.insert_one(recipe_data).inserted_id
-    return {
-        "id": str(recipe)
-    }
+    return {"id": str(recipe)}
 
-def get_single_recipe(id: str) -> dict:
+
+def get_single_recipe(id: str) -> dict | None:
     recipe = recipe_collection.find_one({"_id": ObjectId(id)})
     if recipe:
         return parse_recipe_data(recipe)
+
 
 def get_all_recipes() -> list:
     recipes = []
@@ -35,11 +32,13 @@ def get_all_recipes() -> list:
 
     return recipes
 
+
 def update_recipe_data(id: str, data: dict):
     recipe = recipe_collection.find_one({"_id": ObjectId(id)})
     if recipe:
         recipe_collection.update_one({"_id": ObjectId(id)}, {"$set": data})
         return True
+
 
 def remove_recipe(id: str):
     recipe = recipe_collection.find_one({"_id": ObjectId(id)})
